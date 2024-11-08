289dcda7302281660a91f0725efea936a748e1ae
Jacob <tononjacopo2005@gmail.com>
2024-11-08 08:51:33 +0100

    directory structure + conf BE spring boot

---
+++ Smart Drive/BE/src/main/java/com/project/be/SmartDriveApplication.java
@@ -0,0 +1,13 @@
+package com.project.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDriveApplication.class, args);
+	}
+
+}
